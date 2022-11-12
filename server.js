bdc2e1ed640db52e5286a0c14bbc21da9c2eb1bc
Bishal Budhathoki <deverbishal331@gmail.com>
2022-11-12 18:51:02 +1100

    minor changes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ process.on('SIGINT', () => {
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://username:password@cluster.mongodb.net/invoice?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(process.env.MONGODB_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 MongoClient.connect(process.env.MONGODB_URI || uri, function(err, db)  {
   if (err) throw err;
