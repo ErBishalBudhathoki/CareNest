112d0f0acb06bc30b856d314f86121c70ae4ba21
Bishal Budhathoki <35174206+BishalBudhathoki@users.noreply.github.com>
2026-01-04 22:04:39 +1100

    files added that was mising from tracking

---
+++ lib/app/features/requests/repositories/config_repository.dart
@@ -0,0 +1,42 @@
+import 'package:carenest/backend/api_method.dart';
+import 'package:carenest/app/features/requests/models/job_role_model.dart';
+import 'package:carenest/app/features/requests/models/leave_type_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final configRepositoryProvider = Provider<ConfigRepository>((ref) {
+  return ConfigRepository(ApiMethod());
+});
+
+class ConfigRepository {
+  final ApiMethod _apiMethod;
+
+  ConfigRepository(this._apiMethod);
+
+  Future<List<JobRoleModel>> getJobRoles(String organizationId) async {
+    try {
+      final response = await _apiMethod.get('api/config/job-roles/$organizationId');
+
+      if (response['success'] == true) {
+        final List data = response['data'];
+        return data.map((e) => JobRoleModel.fromJson(e)).toList();
+      }
+      return [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<List<LeaveTypeModel>> getLeaveTypes(String organizationId) async {
+    try {
+      final response = await _apiMethod.get('api/config/leave-types/$organizationId');
+
+      if (response['success'] == true) {
+        final List data = response['data'];
+        return data.map((e) => LeaveTypeModel.fromJson(e)).toList();
+      }
+      return [];
+    } catch (e) {
+      return [];
+    }
+  }
+}
